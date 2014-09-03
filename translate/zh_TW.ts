66c35ea9ab1c5789af59318d9b63b2e63f811f56
kuanyui <azazabc123@gmail.com>
2014-09-03 15:22:39 +0800

    Add Traditional Chinese translation.

---
+++ translate/zh_TW.ts
@@ -0,0 +1,299 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_TW">
+<context>
+    <name>ArchiveFileManager</name>
+    <message>
+        <location filename="stardict-flashcard.py" line="449"/>
+        <source>Current</source>
+        <translation>目前</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="449"/>
+        <source>Filename</source>
+        <translation>檔名</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="449"/>
+        <source>Words</source>
+        <translation>單字數</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="562"/>
+        <source>Create a new archive</source>
+        <translation>建立新的封存檔</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="562"/>
+        <source>Please input a name for new archive file :</source>
+        <translation>請為新的封存檔取個名子：</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="595"/>
+        <source>Filename has already existed, please retry.</source>
+        <translation>檔名已經存在，請重試。</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="587"/>
+        <source>Rename the archive file</source>
+        <translation>重新命名</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="587"/>
+        <source>Please input a new name for this archive file :</source>
+        <translation>請為封存檔輸入新名子：</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="614"/>
+        <source>Please press Ctrl and click to select multiple items first.</source>
+        <translation>必須先按住Ctrl再點選清單，才可以選取多個檔案來合併。</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="625"/>
+        <source>Merge</source>
+        <translation>合併</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="625"/>
+        <source>You are about merging the following files:&lt;br&gt;
+&lt;ol&gt;{0}&lt;/ol&gt;
+Please input new file name for merged file:
+(You cannot use an existed file name)
+</source>
+        <translation>即將合併下列檔案：&lt;br&gt;
+&lt;ol&gt;{0}&lt;/ol&gt;
+請輸入合併後的檔名：
+（不可使用已經存在的檔名）</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="640"/>
+        <source>Filename has existed, please input another one.</source>
+        <translation>檔案已經存在，請重新輸入。</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="652"/>
+        <source>File merged!</source>
+        <translation>合併完成！</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="715"/>
+        <source>Message</source>
+        <translation>訊息</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="660"/>
+        <source>This action will remove all duplicated words in &lt;b&gt;{0}&lt;/b&gt;.&lt;br&gt;
+This action cannot be undone, continue?</source>
+        <translation>這個動作將會移除 &lt;b&gt;{0}&lt;/b&gt;.中所有重複的字詞。&lt;br&gt;
+這個動作無法復原，繼續嗎?</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="685"/>
+        <source>Remove Duplicated Words</source>
+        <translation>移除重複字詞</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="687"/>
+        <source>Done! {0} duplicated word removed.</source>
+        <translation>完成！移除了 {0} 個重複的字詞。</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="694"/>
+        <source>You have to reserve at least one archive file.</source>
+        <translation>你必須保留至少一個封存檔。</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="700"/>
+        <source>Are you sure to delete &lt;b&gt;{0}&lt;/b&gt;?&lt;br&gt;
+                                                   (This action cannot be undone!)</source>
+        <translation>你確定要刪除 &lt;b&gt;{0}&lt;/b&gt; 嗎?&lt;br&gt;
+                                                   (這個動作無法復原！)</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="715"/>
+        <source>Are you sure to import &lt;b&gt;{0} ({1} words)&lt;/b&gt;?</source>
+        <translation>你確定要匯入&lt;b&gt;{0} ({1} 個單字)&lt;/b&gt; 嗎？</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="723"/>
+        <source>Done! {0} words imported.</source>
+        <translation>完成！匯入了 {0} 個單字。</translation>
+    </message>
+</context>
+<context>
+    <name>ArchiveList</name>
+    <message>
+        <location filename="stardict-flashcard.py" line="378"/>
+        <source>Please input filename for archiving the dict,
+or select an existed one:</source>
+        <translation>請輸入封存檔的檔名，
+或者選一個現有的：</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="384"/>
+        <source>Input archive file</source>
+        <translation>輸入封存檔名</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="392"/>
+        <source>Message</source>
+        <translation>訊息</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="392"/>
+        <source>Are you sure to overwrite file &lt;b&gt;{0}&lt;/b&gt;?</source>
+        <translation>你確定要覆寫 &lt;b&gt;{0}&lt;/b&gt; 嗎？</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigWindow</name>
+    <message>
+        <location filename="stardict-flashcard.py" line="419"/>
+        <source>Dict file path:</source>
+        <translation>字典檔路徑：</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="422"/>
+        <source>Memorized count:</source>
+        <translation>背誦次數：</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="431"/>
+        <source>Select Dictionary File Path</source>
+        <translation>選擇字典檔路徑</translation>
+    </message>
+</context>
+<context>
+    <name>HelpWindow</name>
+    <message>
+        <location filename="stardict-flashcard.py" line="734"/>
+        <source>
+&lt;h1&gt;Welcome to &lt;i&gt;Stardict Flashcard&lt;/i&gt;!&lt;/h1&gt;
+You can add new word into flashcard via Stardict with &lt;b style=&apos;background-color: #ddd; color: #333;&apos;&gt;Alt+e&lt;/b&gt;.&lt;br&gt;
+(The words will be added into &lt;i&gt;~/dic.txt&lt;/i&gt; by default)&lt;by&gt;
+Then open &lt;i&gt;Stardict Flashcard&lt;/i&gt;:
+&lt;ol&gt;
+&lt;li&gt;Press &lt;b&gt;Space&lt;/b&gt; to display answer.&lt;/li&gt;
+&lt;li&gt;Then press &lt;b&gt;Enter&lt;/b&gt; means you can recite this word.&lt;/li&gt;
+&lt;li&gt;Or if you can&apos;t think of the word and recite it, press &lt;b&gt;Space&lt;/b&gt; to go on instead.&lt;/li&gt;
+&lt;li&gt;After a word can be recited up to 5 times, the word will be archived into current archive file automatically.&lt;li&gt;
+&lt;/ol&gt;
+
+After finishing all words, you still can review them again by &lt;b&gt;importing archive file&lt;/b&gt;.&lt;br&gt;
+You can manage archive file in &lt;i&gt;File/Manage Archive File&lt;/i&gt;.
+&lt;h2&gt; Need Help? &lt;/h2&gt;
+If you still have problem, you can visit our &lt;a href=&quot;http://www.github.com/kuanyui/stardict-flashcard/&quot;&gt;GitHub&lt;/a&gt; and open an issue.
+&lt;h2&gt; Contribution &lt;/h2&gt;
+Stardict Flashcard is a free software. So if you wish, your contribution is always welcome! Visit &lt;a href=&quot;http://www.github.com/kuanyui/stardict-flashcard/&quot;&gt;GitHub&lt;/a&gt; to see how to.
+
+</source>
+        <translation>
+&lt;h1&gt;歡迎使用 &lt;i&gt;Stardict Flashcard&lt;/i&gt;!&lt;/h1&gt;
+您可以在StarDict中使用 &lt;b style=&apos;background-color: #ddd; color: #333;&apos;&gt;Alt+e&lt;/b&gt; 來給字典檔新增字詞.&lt;br&gt;
+（StarDict 預設會將字詞加進 &lt;i&gt;~/dic.txt&lt;/i&gt; ，您也可以自行修改）&lt;by&gt;
+然後開啟本&lt;i&gt;Stardict Flashcard&lt;/i&gt;:
+&lt;ol&gt;
+&lt;li&gt;按 &lt;b&gt;空白鍵&lt;/b&gt;來顯示答案&lt;/li&gt;
+&lt;li&gt;如果你答對了，按&lt;b&gt;Enter&lt;/b&gt;就代表你答對了這個單字；&lt;/li&gt;
+&lt;li&gt;如果你想不起來這個單字是什麼鬼的話，就按&lt;b&gt;空白鍵&lt;/b&gt;繼續。&lt;/li&gt;
+&lt;li&gt;在一個單字答對 5 次後，該單字將會被自動封存進目前的封存檔中。&lt;li&gt;
+&lt;/ol&gt;
+
+在背完所有單字後，你仍然可以&lt;b&gt;匯入封存檔&lt;/b&gt;來複習它們。&lt;br&gt;
+你可以使用&lt;i&gt;封存&gt;管理&lt;/i&gt;來管理你的封存檔 .
+&lt;h2&gt;需要協助嗎？&lt;/h2&gt;
+如果你還是有疑惑，請至&lt;a href=&quot;http://www.github.com/kuanyui/stardict-flashcard/&quot;&gt;GitHub&lt;/a&gt;並回報問題 （issue）注意請儘量用英文回報。
+&lt;h2&gt; 貢獻 &lt;/h2&gt;
+Stardict Flashcard 是一個自由軟體，歡迎來貢獻一己之力！瀏覽 &lt;a href=&quot;http://www.github.com/kuanyui/stardict-flashcard/&quot;&gt;GitHub&lt;/a&gt;看看要怎麼做。
+
+</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="762"/>
+        <source>Thanks for using Stardict Flashcard!</source>
+        <translation>感謝使用Stardict Flashcard！</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="stardict-flashcard.py" line="235"/>
+        <source>No word remains in dict file now.
+Now you can add new word via StarDict (Alt + e).
+You also can import an archived file to start another reviewing.</source>
+        <translation>目前字典檔中沒有任何字詞。
+你可以透過 StarDict 來新增字詞（在StarDict中按Alt + e）。
+或者匯入封存檔來複習舊字詞。</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="309"/>
+        <source>&amp;Configuration</source>
+        <translation>設定(&amp;C)</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="309"/>
+        <source>Open configuration window.</source>
+        <translation>打開設定視窗。</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="316"/>
+        <source>&amp;Manage</source>
+        <translation>管理(&amp;M)</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="316"/>
+        <source>Create, import, rename, edit, delete archive file.</source>
+        <translation>建立、匯入、重新命名、編輯、刪除封存檔。</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="357"/>
+        <source>&amp;Help</source>
+        <translation>說明(&amp;H)</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="323"/>
+        <source>Open help window.</source>
+        <translation>打開說明視窗。</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="330"/>
+        <source>&amp;Archive Whole Dict</source>
+        <translation>封存字典檔(&amp;A)</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="330"/>
+        <source>Archive all words in dict, then you can import the other archive file.</source>
+        <translation>將字典檔中的所有字詞封存，如此一來就可以再匯入另一個封存檔。</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="336"/>
+        <source>&amp;Open Dict File</source>
+        <translation>開啟字典檔(&amp;O)</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="336"/>
+        <source>Open Dict file with system default editor.</source>
+        <translation>使用系統預設的編輯器開啟字典檔。</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="342"/>
+        <source>&amp;Open Archive Directory</source>
+        <translation>開啟封存目錄(&amp;O)</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="342"/>
+        <source>Open archive directory with external file manager.</source>
+        <translation>使用外部檔案管理員打開封存用目錄。</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="350"/>
+        <source>&amp;File</source>
+        <translation>檔案(&amp;F)</translation>
+    </message>
+    <message>
+        <location filename="stardict-flashcard.py" line="353"/>
+        <source>&amp;Archive</source>
+        <translation>封存(&amp;A)</translation>
+    </message>
+</context>
+</TS>
