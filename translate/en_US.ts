2927bee2615635e167f186f5570692b4cacaf2b6
kuanyui <azazabc123@gmail.com>
2014-09-20 09:23:24 +0800

    An English .ts file for translation usage.

---
+++ translate/en_US.ts
@@ -0,0 +1,337 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>ArchiveFileManager</name>
+    <message>
+        <location filename="main.py" line="462"/>
+        <source>Current</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="462"/>
+        <source>Filename</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="462"/>
+        <source>Words</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="477"/>
+        <source>&amp;New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="478"/>
+        <source>&amp;Rename</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="479"/>
+        <source>&amp;Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="480"/>
+        <source>&amp;Merge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="481"/>
+        <source>&amp;Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="482"/>
+        <source>Remove Duplicated</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="483"/>
+        <source>&amp;Import to Flashcard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="484"/>
+        <source>&amp;Set As Current</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="485"/>
+        <source>&amp;Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="575"/>
+        <source>Create a new archive</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="575"/>
+        <source>Please input a name for new archive file :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="608"/>
+        <source>Filename has already existed, please retry.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="600"/>
+        <source>Rename the archive file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="600"/>
+        <source>Please input a new name for this archive file :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="627"/>
+        <source>Please press Ctrl and click to select multiple items first.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="638"/>
+        <source>Merge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="638"/>
+        <source>You are about merging the following files:&lt;br&gt;
+&lt;ol&gt;{0}&lt;/ol&gt;
+Please input new file name for merged file:
+(You cannot use an existed file name)
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="653"/>
+        <source>Filename has existed, please input another one.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="665"/>
+        <source>File merged!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="698"/>
+        <source>Remove Duplicated Words</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="673"/>
+        <source>This action will remove all duplicated words in &lt;b&gt;{0}&lt;/b&gt;.&lt;br&gt;
+This action cannot be undone, continue?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="700"/>
+        <source>Done! {0} duplicated word removed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="707"/>
+        <source>You have to reserve at least one archive file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="713"/>
+        <source>Delete File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="713"/>
+        <source>Are you sure to delete &lt;b&gt;{0}&lt;/b&gt;?&lt;br&gt;
+                                                   (This action cannot be undone!)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="728"/>
+        <source>Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="728"/>
+        <source>Are you sure to import &lt;b&gt;{0} ({1} words)&lt;/b&gt;?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="736"/>
+        <source>Done! {0} words imported.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ArchiveList</name>
+    <message>
+        <location filename="main.py" line="391"/>
+        <source>Please input filename for archiving Flashcard,
+or select an existed one:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="397"/>
+        <source>Input archive file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="405"/>
+        <source>Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="405"/>
+        <source>Are you sure to overwrite file &lt;b&gt;{0}&lt;/b&gt;?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ConfigWindow</name>
+    <message>
+        <location filename="main.py" line="432"/>
+        <source>Flashcard file path:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="435"/>
+        <source>Memorized count:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="444"/>
+        <source>Select Flashcard File Path</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>HelpWindow</name>
+    <message>
+        <location filename="main.py" line="747"/>
+        <source>
+&lt;h1&gt;Welcome to &lt;i&gt;Stardict Flashcard&lt;/i&gt;!&lt;/h1&gt;
+You can add new word into Flashcard file within Stardict with &lt;span style=&apos;background-color: #afd7ff; color: #005f87; white-space:pre;&apos;&gt; Alt+e &lt;/span&gt;.&lt;br&gt;
+(The words will be added into &lt;i&gt;~/dic.txt&lt;/i&gt; by default, that is just so-called &quot;Flashcard file&quot;)&lt;br&gt;
+After adding some word into Flashcard file, it&apos;s time to startup &lt;i&gt;Stardict Flashcard&lt;/i&gt;:
+&lt;ol&gt;
+&lt;li&gt;Press &lt;span style=&apos;background-color: #afd7ff; color: #005f87; white-space:pre;&apos;&gt; Space &lt;/span&gt; to display answer.&lt;/li&gt;
+&lt;li&gt;Then press &lt;span style=&apos;background-color: #afd7ff; color: #005f87; white-space:pre;&apos;&gt; Enter &lt;/span&gt; means you can recite this word.&lt;/li&gt;
+&lt;li&gt;Or if you can&apos;t think of the word and recite it, press &lt;span style=&apos;background-color: #afd7ff; color: #005f87; white-space:pre;&apos;&gt; Space &lt;/span&gt; to go on instead.&lt;/li&gt;
+&lt;li&gt;After a word can be recited up to 5 times, the word will be archived into current archive file automatically.&lt;/li&gt;
+&lt;li&gt;If a word was added by accident, you can press &lt;span style=&apos;background-color: #afd7ff; color: #005f87; white-space:pre;&apos;&gt; Delete &lt;/span&gt; to remove it.&lt;/li&gt;
+&lt;/ol&gt;
+
+After finishing all words, you still can review them again by &lt;b&gt;importing archive file&lt;/b&gt; back to Flashcard file.&lt;br&gt;
+You can manage archive file in &lt;i&gt;File/Manage Archive File&lt;/i&gt;.
+&lt;h2&gt; Need Help? &lt;/h2&gt;
+If you still have problem, you can visit our &lt;a href=&quot;http://www.github.com/kuanyui/stardict-flashcard/&quot;&gt;GitHub&lt;/a&gt; and open an issue. (If ok, please use English.)
+&lt;h2&gt; Contribution &lt;/h2&gt;
+Stardict Flashcard is a free software. So if you wish, your contribution is always welcome! Visit &lt;a href=&quot;http://www.github.com/kuanyui/stardict-flashcard/&quot;&gt;GitHub&lt;/a&gt; to see what things can do.
+
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="770"/>
+        <source>&amp;Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="776"/>
+        <source>Thanks for using Stardict Flashcard!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="main.py" line="237"/>
+        <source>No word remains in Flashcard file now.
+Now you can add new word via StarDict (Alt + e).
+You also can import an archived file to start another reviewing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="257"/>
+        <source>Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="257"/>
+        <source>Are you exactly sure to remove this word?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="322"/>
+        <source>&amp;Configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="322"/>
+        <source>Open configuration window.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="329"/>
+        <source>&amp;Manage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="329"/>
+        <source>Create, import, rename, edit, delete archive file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="370"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="336"/>
+        <source>Open help window.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="343"/>
+        <source>&amp;Archive Whole Flashcard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="343"/>
+        <source>Archive all words in Flashcard, then you can import the other archive file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="349"/>
+        <source>&amp;Open Flashcard File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="349"/>
+        <source>Open Flashcard file with system default editor.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="355"/>
+        <source>&amp;Open Archive Directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="355"/>
+        <source>Open archive directory with external file manager.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="363"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="366"/>
+        <source>&amp;Archive</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
