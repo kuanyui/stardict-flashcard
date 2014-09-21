a4d7e6fec1f525f2ef7c412ed318b0a63651be55
kuanyui <azazabc123@gmail.com>
2014-09-21 16:24:21 +0800

    Improve the appearance of tooltip.

--- main.py
+++ main.py
@@ -23,12 +23,12 @@ def sdcv(word):
             # print(formattedOut)
     return formattedOut
 
-class LookupTooltip(QtGui.QWidget):
+class LookupTooltip(QtGui.QDialog):
     def __init__(self, parent = None):
         super().__init__(parent)
         self.setWindowFlags(QtCore.Qt.ToolTip)
         add_button = IconButton('c', 'add.png', self.tr('&Add This Word'), self.addWord)
-        add_button.setStyleSheet('''QPushButton{border: none;margin: 3;}
+        add_button.setStyleSheet('''QPushButton{border: none;margin: 3px;padding:5px}
         QPushButton:pressed {
         background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 rgba(0, 0, 0, 50), stop: 1 rgba(255,255,255,50));
         border: 1px solid rgba(255,255,255,50);
@@ -40,19 +40,19 @@ class LookupTooltip(QtGui.QWidget):
         border-radius: 3px;
         }
         ''')
-        add_button.resize(0, 50)
 
         self.description_browser = QtGui.QTextBrowser()
         self.description_browser.setFrameShape(QtGui.QFrame.NoFrame)
 
         layout = QtGui.QVBoxLayout()
-        layout.setMargin(0)
+        layout.setMargin(1)
         layout.setSpacing(0)
-
         layout.addWidget(add_button)
         layout.addWidget(self.description_browser)
         self.setLayout(layout)
         self.adjustSize()
+        self.setStyleSheet('''QDialog{border: 1px solid rgba(255, 255, 255, 50);}''')
+
         # Auto close() when mouse move out of tooltip
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.checkMousePosition)
@@ -61,7 +61,7 @@ class LookupTooltip(QtGui.QWidget):
     def lookupAndShow(self, word):
         self.word = word
         self.description_browser.setText("Lookup...")
-        self.move(QtGui.QCursor().pos().x() - 2, QtGui.QCursor().pos().y() - 2) # <= abs pos
+        self.move(QtGui.QCursor().pos().x() + 10, QtGui.QCursor().pos().y() + 10) # <= abs pos
         self.show()
         self.description_browser.setHtml(sdcv(word))
 
