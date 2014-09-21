6cb5bdcd6b0c706df186b28ff88a704ec100ebd5
kuanyui <azazabc123@gmail.com>
2014-09-21 15:28:37 +0800

    Auto close() when mouse is out of tooltip.

--- main.py
+++ main.py
@@ -27,21 +27,36 @@ class LookupTooltip(QtGui.QWidget):
     def __init__(self, parent = None):
         super().__init__(parent)
         self.setWindowFlags(QtCore.Qt.ToolTip)
-        close_button = IconButton('c', 'delete.png', self.tr('&Close'), self.close)
         add_button = IconButton('c', 'add.png', self.tr('&Add This Word'), self.addWord)
-        add_button.adjustSize()
+        add_button.setStyleSheet('''QPushButton{border: none;margin: 3;}
+        QPushButton:pressed {
+        background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 rgba(0, 0, 0, 50), stop: 1 rgba(255,255,255,50));
+        border: 1px solid rgba(255,255,255,50);
+        border-radius: 3px;
+        }
+        QPushButton:hover:!pressed {
+        background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 rgba(255,255,255,60), stop: 1 rgba(0,0,0,20));
+        border: 1px solid rgba(255,255,255,50);
+        border-radius: 3px;
+        }
+        ''')
+        add_button.resize(0, 50)
+
         self.description_browser = QtGui.QTextBrowser()
         self.description_browser.setFrameShape(QtGui.QFrame.NoFrame)
 
-        layout = QtGui.QGridLayout()
+        layout = QtGui.QVBoxLayout()
         layout.setMargin(0)
         layout.setSpacing(0)
 
-        layout.addWidget(close_button, 0, 0)
-        layout.addWidget(add_button, 0, 1)
-        layout.addWidget(self.description_browser, 1,0,1,2)
+        layout.addWidget(add_button)
+        layout.addWidget(self.description_browser)
         self.setLayout(layout)
         self.adjustSize()
+        # Auto close() when mouse move out of tooltip
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.checkMousePosition)
+        self.timer.start(300)
 
     def lookupAndShow(self, word):
         self.word = word
@@ -57,6 +72,17 @@ class LookupTooltip(QtGui.QWidget):
         QtCore.QTimer.singleShot(1000, lambda: self.parentWidget().statusBar().clearMessage())
         self.close()
 
+    def checkMousePosition(self):
+        pos = self.pos()
+        size = self.size()
+        x = pos.x()
+        y = pos.y()
+        h = size.height()
+        w = size.width()
+        mouse = QtGui.QCursor.pos()
+        if (mouse.x() > x + w + 20 or mouse.x() < x - 20 or
+            mouse.y() > y + h + 20 or mouse.y() < y - 20):
+            self.close()
         
 class ConfigFile():
     def __init__(self, parent=None):
