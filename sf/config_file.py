e787dc76200a133b0ba1d5373375d1edd9d3cb54
kuanyui <azazabc123@gmail.com>
2015-08-25 15:44:04 +0800

    refactoring.

--- sf/config_file.py
+++ sf/config_file.py
@@ -3,20 +3,18 @@ import sys, os, math, configparser
 from PyQt4 import QtCore, QtGui
 from .main import *
 
-ROOT = os.path.expanduser('~/.stardict-flashcard/')
-CONFIG_PATH = os.path.join(ROOT, 'rc.ini')
-ARCHIVE_DIR = os.path.join(ROOT, 'archive')
-ACT_ICON_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'icons/actions/')
-
-OPEN_FIRST_TIME_HELP = False
-
 class ConfigFile():
+    ROOT = os.path.expanduser('~/.stardict-flashcard/')
+    CONFIG_PATH = os.path.join(ROOT, 'rc.ini')
+    ARCHIVE_DIR = os.path.join(ROOT, 'archive')
+    ACT_ICON_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'icons/actions/')
+    OPEN_FIRST_TIME_HELP = False
+    
     def __init__(self, parent=None):
         if not os.path.isdir(ROOT):
             os.makedirs(ROOT)
         if not os.path.isfile(CONFIG_PATH):
-            global OPEN_FIRST_TIME_HELP
-            OPEN_FIRST_TIME_HELP = True
+            self.OPEN_FIRST_TIME_HELP = True
             with open(CONFIG_PATH, 'w') as file:
                 file.write('''[Path]
 FlashcardPath = ~/dic.txt
@@ -40,22 +38,23 @@ FirstTimeHelp = True
             self.__init__()
             
     def initializeGlobalVar(self):
-        global FLASHCARD_PATH, ARCHIVE_FILE_NAME, ARCHIVE_FILE_FULLNAME, MEMORIZED_COUNT, REMEMBER_INDEX
-        FLASHCARD_PATH        = os.path.expanduser(self.parser['Path']['FlashcardPath'])
-        ARCHIVE_FILE_NAME     = self.parser['Path']['ArchiveFileName']
-        ARCHIVE_FILE_FULLNAME = os.path.join(ARCHIVE_DIR, ARCHIVE_FILE_NAME)
-        MEMORIZED_COUNT       = int(self.parser['Main']['MemorizedCount'])
-        REMEMBER_INDEX        = int(self.parser['Main']['RememberIndex'])
+        self.FLASHCARD_PATH        = os.path.expanduser(self.parser['Path']['FlashcardPath'])
+        self.ARCHIVE_FILE_NAME     = self.parser['Path']['ArchiveFileName']
+        self.ARCHIVE_FILE_FULLNAME = os.path.join(self.ARCHIVE_DIR, self.ARCHIVE_FILE_NAME)
+        self.MEMORIZED_COUNT       = int(self.parser['Main']['MemorizedCount'])
+        self.REMEMBER_INDEX        = int(self.parser['Main']['RememberIndex'])
         # if Archive dir & file not exist, create
-        if not os.path.isdir(ARCHIVE_DIR):
-            os.makedirs(ARCHIVE_DIR)
+        if not os.path.isdir(self.ARCHIVE_DIR):
+            os.makedirs(self.ARCHIVE_DIR)
         if not os.path.exists(ARCHIVE_FILE_FULLNAME):
             open(ARCHIVE_FILE_FULLNAME, 'a').close() # touch current archive file
 
     def writeConfigFile(self):
-        self.parser['Path']['FlashcardPath']   = FLASHCARD_PATH
-        self.parser['Path']['ArchiveFileName'] = ARCHIVE_FILE_NAME
-        self.parser['Main']['MemorizedCount']  = str(MEMORIZED_COUNT)
-        self.parser['Main']['RememberIndex']   = str(REMEMBER_INDEX)
+        self.parser['Path']['FlashcardPath']   = self.FLASHCARD_PATH
+        self.parser['Path']['ArchiveFileName'] = self.ARCHIVE_FILE_NAME
+        self.parser['Main']['MemorizedCount']  = str(self.MEMORIZED_COUNT)
+        self.parser['Main']['RememberIndex']   = str(self.REMEMBER_INDEX)
         with open(CONFIG_PATH, 'w') as file:
             self.parser.write(file)
+
+
